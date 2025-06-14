21db31b512280cd5b9c2304b8a59dbdd65651f77
PWR <pwrpntr@gmail.com>
2025-06-14 11:12:54 +0200

    update blocktext rendering

--- packages/sveltekit/src/lib/modules/sanity/index.ts
+++ packages/sveltekit/src/lib/modules/sanity/index.ts
@@ -1,62 +1,47 @@
 import { createClient } from "@sanity/client"
-import blocksToHtml from '@sanity/block-content-to-html'
+import { toHTML } from '@portabletext/to-html'
 import imageUrlBuilder from '@sanity/image-url'
 import { SANITY_ID } from "$lib/constants";
+import type { PortableTextBlock } from '@portabletext/types'
 
 export const client = createClient({
     projectId: SANITY_ID,
     dataset: 'production',
     token: '', // or leave blank to be anonymous user
     useCdn: false, // `false` if you want to ensure fresh data
-    apiVersion: '2024-08-14',
+    apiVersion: '2025-06-01',
 })
 
-const h = blocksToHtml.h
-
-export const renderBlockText = (blocks: any) =>
-    blocksToHtml({
-        blocks: blocks,
-        serializers: serializers,
-        projectId: SANITY_ID,
-        dataset: 'production',
-    })
-
-export const toPlainText = (blocks: any) => {
-    return (
-        blocks
-            .map(block => {
-                if (block._type !== 'block' || !block.children) {
-                    return ''
+export const renderBlockText = (blocks: PortableTextBlock[]) => {
+    return toHTML(blocks, {
+        components: {
+            marks: {
+                link: ({ children, value }) => {
+                    return `<a href="${value.href}" target="_blank" rel="noreferrer">${children}</a>`
                 }
-                return block.children.map(child => child.text).join('')
-            })
-            .join('\n\n')
-    )
+            },
+            block: {
+                normal: ({ children }) => `<p>${children}</p>`,
+                blockquote: ({ children }) => `<blockquote>${children}</blockquote>`,
+            }
+        }
+    })
+}
+
+export const toPlainText = (blocks: PortableTextBlock[]) => {
+    return blocks
+        .map(block => {
+            if (block._type !== 'block' || !block.children) {
+                return ''
+            }
+            return block.children.map(child => child.text).join('')
+        })
+        .join('\n\n')
 }
 
 const builder = imageUrlBuilder(client)
 
 export const urlFor = (source: any) => builder.image(source)
-
-const serializers = {
-    marks: {
-        link: props =>
-            h(
-                'a',
-                { target: '_blank', rel: 'noreferrer', href: props.mark.href },
-                props.children
-            )
-    },
-    types: {
-        block: props => {
-            const style = props.node.style || 'normal'
-            return style === 'blockquote'
-                ? h('blockquote', {}, props.children)
-                : h('p', { className: style }, props.children)
-
-        },
-    }
-}
 
 export const loadData = async (query: string, params: any) => {
     try {
