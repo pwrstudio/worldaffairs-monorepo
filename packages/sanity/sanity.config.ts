3ffae80c971505cf8156d7b2533976091523d288
PWR <pwrpntr@gmail.com>
2025-08-01 21:32:33 +0200

    work on singleton types

--- packages/sanity/sanity.config.ts
+++ packages/sanity/sanity.config.ts
@@ -3,6 +3,12 @@ import { structureTool } from 'sanity/structure'
 import { schemaTypes } from './schemaTypes'
 import deskStructure from './deskStructure'
 
+const SINGLETON_TYPES = [
+  'about',
+  'newPosts',
+  'storeList'
+];
+
 export default defineConfig({
   name: 'default',
   title: 'worldaffairs-admin',
@@ -16,24 +22,23 @@ export default defineConfig({
   document: {
     newDocumentOptions: (prev, { creationContext }) => {
       const { type } = creationContext;
-      const DISABLED_TYPES = [
-        'about',
-        'newPosts',
-        'storeList'
-      ];
+
       if (type === 'global') {
-        return prev.filter((template) => !DISABLED_TYPES.includes(template.templateId));
+        return prev.filter((template) => !SINGLETON_TYPES.includes(template.templateId));
       }
       return prev;
     },
     
-    // Prevent deletion of certain document types
+    // Prevent deletion, duplication, and unpublishing of certain document types
     actions: (prev, { schemaType }) => {
-      const PROTECTED_TYPES = ['about', 'newPosts', 'storeList', 'release'];
       
-      if (PROTECTED_TYPES.includes(schemaType)) {
-        // Remove the delete action for protected document types
-        return prev.filter(action => action.action !== 'delete');
+      if (SINGLETON_TYPES.includes(schemaType)) {
+        // Remove the delete, duplicate, and unpublish actions for protected document types
+        return prev.filter(action => 
+          action.action !== 'delete' && 
+          action.action !== 'duplicate' && 
+          action.action !== 'unpublish'
+        );
       }
       
       return prev;
